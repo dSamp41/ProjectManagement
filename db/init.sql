bec91ff192b27b3778e63d79633f278e7341f59d
dSamp41 <d.sampietro@studenti.unipi.it>
2024-03-15 17:54:05 +0100

    Create init.sql

---
+++ db/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE fst (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20),
+    age INTEGER
+);
+
+INSERT INTO fst (name, age) VALUES
+    ('AAA', 16),
+    ('BBB', 21),
+    ('CCC', 36),
